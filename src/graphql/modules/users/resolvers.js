31367f208125733a64f3fe4c99b35b117752be49
Diego <dronan@gmail.com>
2020-10-07 14:58:16 -0300

    crud

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,16 @@
+import User from '../../../models/User'
+
 export default {
-  Query: {},
-  Mutation: {}
-}
+  Query: {
+    users: () => User.find(),
+    user: (_, { id}) => User.findById(id)
+  },
+  Mutation: {
+    createUser: (_, { data }) => User.create(data),
+    updateUser: (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
+    deleteUser: async (_, { id }) => {
+      const deleted = await User.findByIdAndDelete(id);
+      return !!deleted;
+    },
+  },
+};
