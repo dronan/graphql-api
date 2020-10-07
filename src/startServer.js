acc0fa383f227649e975ae4fde864ec2e0f5b119
Diego <dronan@gmail.com>
2020-10-07 16:10:41 -0300

    adicionando subscription para notificacoes e websocket para cadastro

--- src/startServer.js
+++ src/startServer.js
@@ -1,4 +1,4 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, PubSub } from 'apollo-server';
 import mongoose from 'mongoose';
 
 function startServer({typeDefs, resolvers}) {
@@ -6,9 +6,10 @@ function startServer({typeDefs, resolvers}) {
   mongoose.connect( 'mongodb://localhost:27017/graphql', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
+  });
 
-  const server = new ApolloServer({typeDefs, resolvers});
+  const pubsub = new PubSub();
+  const server = new ApolloServer({typeDefs, resolvers, context: { pubsub } });
   server.listen().then(({ url }) => console.log(`Server Started at ${url}`));
 }
 
